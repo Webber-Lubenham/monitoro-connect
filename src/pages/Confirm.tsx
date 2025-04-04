271c36290c008bfd442aa52db1c26234c04a2847
Webber-Lubenham <frank.webber.dev@gmail.com>
2025-04-04 18:27:56 +0100

    para testar

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,5 +1,6 @@
-import { ConfirmationLoading } from 'components/auth/confirmation/ConfirmationLoading';
-import { ConfirmationError } from 'components/auth/confirmation/ConfirmationError';
+import React from 'react';
+import { ConfirmationLoading } from '../components/auth/confirmation/ConfirmationLoading.tsx';
+import { ConfirmationError } from '../components/auth/confirmation/ConfirmationError.tsx';
 
 // Other existing code in the file...
 
@@ -8,7 +9,7 @@ const Confirm = () => {
   return (
     <div>
       <ConfirmationLoading />
-      <ConfirmationError />
+      <ConfirmationError error="An error occurred" />
       {/* Other components or content */}
     </div>
   );
